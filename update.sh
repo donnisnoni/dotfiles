930ab8c7ddb0038bb152c1138a9a5ea7a13afb28
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-22 17:48:14 +0800

    Add .netrc file & ignore .npmrc for now

--- update.sh
+++ update.sh
@@ -5,5 +5,6 @@ cp $HOME/.bash_profile              ./home
 # cp $HOME/.bash_logout             ./home
 cp $HOME/.gitconfig                 ./home
 cp $HOME/.gitignore_global          ./home
-cp $HOME/.npmrc                     ./home
+# cp $HOME/.npmrc                     ./home
+cp $HOME/.netrc                     ./home
 cp $HOME/.config/fish -rf           ./home/.config/fish
