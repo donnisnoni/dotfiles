004739a9503338d147f16c7faae1ed120eb403a2
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-22 17:17:55 +0800

    Add update.sh to update the files

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/sh
 
 # HOME config files
-cp $HOME/.bash_profile    ./home
-cp $HOME/.bash_logout     ./home
-cp $HOME/.gitconfig       ./home
-cp $HOME/.npmrc           ./home
-cp $HOME/.config/fish -rf  ./home/.config/fish
+cp $HOME/.bash_profile              ./home
+# cp $HOME/.bash_logout             ./home
+cp $HOME/.gitconfig                 ./home
+cp $HOME/.gitignore_global          ./home
+cp $HOME/.npmrc                     ./home
+cp $HOME/.config/fish -rf           ./home/.config/fish
