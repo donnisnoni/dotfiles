beead73d85198660f758f028caa713c82c93d38e
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-22 17:36:25 +0800

    Update dotfiles

--- home/.bash_profile
+++ home/.bash_profile
@@ -1,2 +1,6 @@
 # PS1="🔥"
 # This loads nvm bash_completion
+
+PATH="/home/don/.local/bin:$PATH"
+HH=donexport GPG_TTY=$(tty)
+export GPG_TTY=$(tty)
--- home/.config/fish/fish/config.fish
+++ home/.config/fish/fish/config.fish
@@ -1,20 +1,51 @@
 #/usr/bin/fish
 
 # Load Powerline
-set fish_function_path $fish_function_path "/home/don/.local/lib/python2.7/site-packages/powerline/bindings/fish"
-powerline-setup
+# set fish_function_path $fish_function_path "/home/don/.local/lib/python2.7/site-packages/powerline/bindings/fish"
+# powerline-setup
 
 # Extend PATH environment variable
-set PATH ~/.local/bin:/media/don/DEV/Apps/node/v12.8.1/bin:$PATH
+set donexport GPG_TTY=(tty)
 
 # Load icons-in-terminal
-source ~/.local/share/icons-in-terminal/icons.fish
+# source ~/.local/share/icons-in-terminal/icons.fish
 
 # Aliases
-alias gclone "git clone"
+alias gclone 		"git clone"
+alias cdv			"cd /media/don/DEV/Projects/v/vlang"
+alias r 			"reset"
+alias emsdk_setup 	". /media/don/DEV/Apps/emsdk/emsdk_env.fish"
+alias vup			"cdv && git checkout master && git pull --rebase && ./v -cc clang -prod ./cmd/v/v.v -o  v && ls -sh1 && cd -"
+alias lampp_env		"set PATH /opt/lampp:/opt/lampp/bin:$PATH"
+alias projects  	"cd /media/don/DEV/Projects/"
+alias vprojects		"cd /media/don/DEV/Projects/v"
+alias kernel_lists	"sudo dpkg --list | egrep -i --color 'linux-image|linux-headers'"
+alias chrome_minimal "/opt/google/chrome/chrome --disable-3d-apis --disable-2d-canvas-clip-aa --disable-2d-canvas-image-chromium --disable-breakpad --disable-canvas-aa --start-maximized --disable-crash-reporter --disable-default-apps --disable-notifications --disable-sync --disable-webgl"
+# --enable-leak-detection --minimal --native
+# https://play.famobi.com/table-tennis-world-tour
 
-# When logout event
-function on_exit --on-process %self
-    rm -rf ~/.npm/*
-    rm -rf ~/.cache/Homebrew/*
+# export VFLAGS="-cc tcc -cg"
+export CC="clang"
+
+export GPG_TTY=(tty)
+
+# source /home/don/.local/share/omf/themes/default/fish_prompt.fish
+
+# function fish_prompt
+#     powerline-shell --shell bare $status
+# end
+
+function delete_all_exec
+	find . -maxdepth 1 -type f -executable -delete 
 end
+
+# Load NVM
+function load_nvm
+	set NVM_DIR '/media/don/DEV/Apps/nvm'
+	set PATH $NVM_DIR:$PATH	
+end
+
+set NODE_PATH /media/don/DEV/Apps/nvm/versions/node/v14.15.1/bin
+#nvm use --delete-prefix v14.15.1
+#set PATH $NODE_PATH:$PATH
+#neofetch
